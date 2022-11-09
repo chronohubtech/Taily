737d354fe29052bfb72565b3e2e500f5c80ee4b8
Kurono <chrono.social@outlook.com>
2022-11-09 11:49:46 +0800

    Revert ":wrench: Aliases added"
    This reverts commit 5f8cbae4f07b75df2025774e4f931df9cbd9f806.

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@routes': path.resolve(__dirname, './src/routes'),
-      '@assets': path.resolve(__dirname, './src/assets')
-    }
-  }
+  base: ''
 });
