2761642c87731a03f26bef73aa0c285d5b8ae15b
Justin Pascual <chrono.social@outlook.com>
2022-11-10 16:23:48 +0800

    :children_crossing: Redirect to create account page

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,9 +1,21 @@
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+// Static assets
+import VerticalLogo from '@assets/static/vertical-logo.svg';
+
 function Root() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    return () => {
+      setTimeout(() => navigate('create-account'), 2000);
+    };
+  }, []);
+
   return (
-    <>
-      <Link to={'create-account'}>Create account</Link>
-    </>
+    <main className={'flex items-center justify-center h-screen'}>
+      <img src={VerticalLogo} width={181} height={241} alt="Taily vertical logo" />
+    </main>
   );
 }
 
