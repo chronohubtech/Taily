e92e9c3dd788a9ded0f9940a38e62719bd82338b
Justin Pascual <chrono.social@outlook.com>
2022-11-10 13:33:10 +0800

    :sparkles: Password visibility toggle added

--- src/components/input-field/input-field.component.jsx
+++ src/components/input-field/input-field.component.jsx
@@ -1,6 +1,17 @@
+import { useState } from 'react';
 import './input-field.style.css';
 
 export function InputField({ label, ...otherProps }) {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const passwordVisibility = (event) => {
+    event.preventDefault();
+
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
+  const isInputTypePassword = otherProps.type === 'password';
+
   return (
     <div className={'input__container'}>
       <label htmlFor="username">{label}</label>
@@ -12,7 +23,17 @@ export function InputField({ label, ...otherProps }) {
         spellCheck="false"
         data-gramm="false"
         data-ms-editor="false"
+        type={isInputTypePassword ? (isPasswordVisible ? 'text' : 'password') : otherProps.type}
       />
+
+      {isInputTypePassword && (
+        <button
+          onClick={passwordVisibility}
+          className={'password-toggle'}
+          title={isPasswordVisible ? 'Show password' : 'Hide password'}>
+          {isPasswordVisible ? '🔓' : '🔒'}
+        </button>
+      )}
     </div>
   );
 }
