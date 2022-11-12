bc0cf2b2fdd7ab88573571b7c68691512f24370f
Kurono <chrono.social@outlook.com>
2022-11-12 14:21:37 +0800

    :lipstick: Home header added - home style css added

--- src/routes/home/home.route.jsx
+++ src/routes/home/home.route.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
+import './home.style.css';
 
 function HomeRoute() {
   return (
     <div>
-      <h2>Home page</h2>
+      <section className={'home__section'}>
+        <motion.div
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.3 }}
+          className={'home__container'}>
+          <div className={'home__header'}>
+            <div className={'home__title'}>
+              <h3>Tasks</h3>
+              <p>Tuesday, November 8</p>
+            </div>
+          </div>
+        </motion.div>
+      </section>
     </div>
   );
 }
