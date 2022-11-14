687aebc97fbbf479c1a1adfe63295a2ddf6daee4
Kurono <chrono.social@outlook.com>
2022-11-15 00:40:04 +0800

    Firebase config added

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.VITE_API_KEY,
+  authDomain: process.env.VITE_AUTH_DOMAIN,
+  projectId: process.env.VITE_PROJECT_ID,
+  storageBucket: process.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_SENDER_ID,
+  appId: process.env.VITE_APP_ID
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+console.log(firebaseApp);
